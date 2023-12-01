5671d93914248e16f4788a388627bf256da2cd5e
Rishabh Kumar <rishabhkumar@Rishabhs-MacBook-Pro.local>
2023-12-01 17:44:30 +0530

    Using & Outputting Dynamic Values [Core Concept]

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+const Data = ['Fundamental', 'Crucial', 'Core'];
+function getRandomData(len){
+  return Math.floor(Math.random()*len);
+}
 function Header() {
+  let varableData = Data[getRandomData(3)];
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {varableData} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
