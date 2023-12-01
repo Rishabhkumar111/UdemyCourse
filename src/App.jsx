ad18576c1340f41528f8cfb2004b7382aa61f032
Rishabh Kumar <rishabhkumar@Rishabhs-MacBook-Pro.local>
2023-12-01 23:15:34 +0530

    Outputting dynamic content and making reusable component with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import reactImg from './assets/react-core-concepts.png'
+import reactImg from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data.jsx";
 
-const Data = ['Fundamental', 'Crucial', 'Core'];
-function getRandomData(len){
-  return Math.floor(Math.random()*len);
+const Data = ["Fundamental", "Crucial", "Core"];
+function getRandomData(len) {
+  return Math.floor(Math.random() * len);
 }
 function Header() {
   let varableData = Data[getRandomData(3)];
@@ -17,12 +18,43 @@ function Header() {
     </header>
   );
 }
+
+function CoreConcept(prop) {
+  return (
+    <li>
+      <img src={prop.image} alt={prop.title} />
+      <h3>{prop.title}</h3>
+      <p>{prop.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              image={CORE_CONCEPTS[0].image}
+              description={CORE_CONCEPTS[0].description}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[1].title}
+              image={CORE_CONCEPTS[1].image}
+              description={CORE_CONCEPTS[1].description}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[2].title}
+              image={CORE_CONCEPTS[2].image}
+              description={CORE_CONCEPTS[2].description}
+            />
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
       </main>
     </div>
   );
